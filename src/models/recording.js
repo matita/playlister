a44885a76386e0780e06a66e72bac37551a6f1a7
Matita <matitald@gmail.com>
2016-08-12 14:42:29 +0200

    scoring source match with duration, lyrics, live

--- src/models/recording.js
+++ src/models/recording.js
@@ -20,7 +20,9 @@ module.exports = function (props) {
           return callback(err)
 
         result.items.forEach(mergeParts(results))
-        props.sources = results.sort(sortByDuration)
+        props.sources = results
+          .map(withScore)
+          .sort(sortByScore)
         callback(null, props.sources)
       })
     })
@@ -29,6 +31,30 @@ module.exports = function (props) {
   function getDurationDelta(v) {
     var videoDuration = durationToSeconds(v.contentDetails.duration) * 1000
     return Math.abs(videoDuration - props.length)
+  }
+
+  function withScore(source) {
+    var sourceTitle = source.title.toLowerCase()
+    var recordingTitle = props.title.toLowerCase()
+
+    var sourceHasLyrics = sourceTitle.indexOf('lyrics') !== -1
+    var recordingHasLyrics = recordingTitle.indexOf('lyrics') !== -1
+    var lyricsScore = sourceHasLyrics && !recordingHasLyrics ? 100 : 0
+
+    var sourceIsLive = sourceTitle.indexOf('live') !== -1
+    var recordingIsLive = recordingTitle.indexOf('live') !== -1
+    var liveScore = sourceIsLive && !recordingIsLive ? -100 : 0
+
+    var durationDelta = getDurationDelta(source) / 1000
+    var durationScore = isNaN(props.length) ? 0 : 50 / (durationDelta || 1)
+
+    source.score = lyricsScore + liveScore + durationScore
+    
+    return source
+  }
+
+  function sortByScore(s1, s2) {
+    return s2.score - s1.score
   }
 
   function sortByDuration(v1, v2) {
