89dc2181dc2c8d976f340e2be34a574b217f33e7
Matita <matitald@gmail.com>
2019-01-01 20:53:32 +0100

    try to optimize seekbar for touch screen

--- src/components/Seekbar.js
+++ src/components/Seekbar.js
@@ -18,6 +18,14 @@ export default class Seekbar extends React.Component {
         this.handleMouseMove(e);
     }
 
+    handleTouchStart = (e) => {
+        console.log('touchstart');
+        window.addEventListener('touchmove', this.handleMouseMove, false);
+        window.addEventListener('touchend', this.handleTouchEnd, false);
+        this.setState({ isDragging: true });
+        this.handleMouseMove(e);
+    }
+
     handleMouseMove = (e) => {
         e.preventDefault();
         const x = e.clientX - this.el.offsetLeft;
@@ -38,13 +46,25 @@ export default class Seekbar extends React.Component {
             onChange({ target: { value: dragValue } });
     }
 
+    handleTouchEnd = (e) => {
+        const { onChange } = this.props;
+        const { dragValue } = this.state;
+        
+        window.removeEventListener('touchmove', this.handleMouseMove);
+        window.removeEventListener('touchend', this.handleTouchEnd);
+        this.setState({ isDragging: false });
+        
+        if (onChange)
+            onChange({ target: { value: dragValue } });
+    }
+
     render() {
         const { value, total, style } = this.props;
         const { isDragging, dragValue } = this.state;
         const thumbValue = isDragging ? dragValue : value;
 
         return (
-            <div className={'seekbar' + (isDragging ? ' dragging' : '')} ref={this.handleRef} style={style} onMouseDown={this.handleMouseDown}>
+            <div className={'seekbar' + (isDragging ? ' dragging' : '')} ref={this.handleRef} style={style} onMouseDown={this.handleMouseDown} onTouchStart={this.handleTouchStart}>
                 <div className="seekbar-background">
                     <div className="seekbar-value" style={{ width: `${thumbValue/total*100}%` }}></div>
                 </div>
